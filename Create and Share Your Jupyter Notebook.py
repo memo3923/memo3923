12a145eb0dc8aa3971b154f47241ce2b3614c8e5
memo3923 <117024749+memo3923@users.noreply.github.com>
2022-10-31 12:34:49 +0200

    Python1 project

---
+++ Create and Share Your Jupyter Notebook.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### My Jupyter Notebook on IBM Watson Studio
+
+# **Mai Ramadan**
+
+# I want to be data scientist
+
+# *Iam interested in data science because, I love solving the problem related to data, learn new skills, will help me in my biophysics Ph.D studies.*
+
+# ### This code for sum two numbers:
+
+# In[2]:
+
+
+num1=5
+num2=9
+print(num1+num2)
+
+
+# 1. One
+# 2. Two  
+# 3. Three
+# 4. Four
+# 5. Five
+
+# |Class 1|Class 2|
+# |-------|-------|
+# | 30 st.|  35st.|
+
+# In[ ]:
+
+
+
+
